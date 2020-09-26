de1dae698058b166f231942bc36539740d372982
Filipe Machado <filipwx7@gmail.com>
2020-09-26 15:48:18 -0300

    initial coin

---
+++ mac-release-package.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+VERSION=$1
+if [ -x ${VERSION} ];
+then
+	echo VERSION not defined
+	exit 1
+fi
+PACKAGE=RCB-client-${VERSION}
+echo PACKAGE="${PACKAGE}"
+CHANGELOG=RCB-client-${VERSION}.changelog.txt
+OBFUSCATE=$2
+MACVERSION=$3
+if [ -x ${MACVERSION} ];
+then
+MACVERSION=${VERSION}
+fi
+echo MACVERSION="${MACVERSION}"
+
+FILES="changelogs conf html lib resource contrib"
+FILES="${FILES} RealCoin.exe RealCoinservice.exe"
+FILES="${FILES} 3RD-PARTY-LICENSES.txt AUTHORS.txt LICENSE.txt"
+FILES="${FILES} DEVELOPERS-GUIDE.md OPERATORS-GUIDE.md README.md README.txt USERS-GUIDE.md"
+FILES="${FILES} mint.bat mint.sh run.bat run.sh run-tor.sh run-desktop.sh start.sh stop.sh compact.sh compact.bat sign.sh"
+FILES="${FILES} nxt.policy nxtdesktop.policy Wallet.url Dockerfile"
+
+echo compile
+./compile.sh
+rm -rf html/doc/*
+rm -rf nxt
+rm -rf ${PACKAGE}.jar
+rm -rf ${PACKAGE}.exe
+rm -rf ${PACKAGE}.zip
+mkdir -p nxt/
+mkdir -p nxt/logs
+mkdir -p nxt/addons/src
+
+if [ "${OBFUSCATE}" = "obfuscate" ]; 
+then
+echo obfuscate
+~/proguard/proguard5.2.1/bin/proguard.sh @nxt.pro
+mv ../nxt.map ../nxt.map.${VERSION}
+else
+FILES="${FILES} classes src COPYING.txt"
+FILES="${FILES} compile.sh javadoc.sh jar.sh package.sh"
+FILES="${FILES} win-compile.sh win-javadoc.sh win-package.sh"
+echo javadoc
+./javadoc.sh
+fi
+echo copy resources
+cp installer/lib/JavaExe.exe RealCoin.exe
+cp installer/lib/JavaExe.exe RealCoinservice.exe
+cp -a ${FILES} nxt
+cp -a logs/placeholder.txt nxt/logs
+echo gzip
+for f in `find nxt/html -name *.gz`
+do
+	rm -f "$f"
+done
+for f in `find nxt/html -name *.html -o -name *.js -o -name *.css -o -name *.json  -o -name *.ttf -o -name *.svg -o -name *.otf`
+do
+	gzip -9c "$f" > "$f".gz
+done
+cd nxt
+echo generate jar files
+../jar.sh
+echo package installer Jar
+../installer/build-installer.sh ../${PACKAGE}
+cd -
+rm -rf nxt
+
+echo bundle a dmg file	
+/Library/Java/JavaVirtualMachines/jdk-14.0.1.jdk/Contents/Home/bin/jpackage -deploy -outdir . -outfile RCB-client -name RCB-installer -width 34 -height 43 -native dmg -srcfiles ${PACKAGE}.jar -appclass com.izforge.izpack.installer.bootstrap.Installer -v -Bmac.category=Business -Bmac.CFBundleIdentifier=org.nxt.client.installer -Bmac.CFBundleName=RCB-Installer -Bmac.CFBundleVersion=${MACVERSION} -BappVersion=${MACVERSION} -Bicon=installer/AppIcon.icns > installer/javapackager.log 2>&1
